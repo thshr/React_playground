a74bd9a6c179f0054a9a9895bf31526584ff5e40
thshr <ths_hr@hotmail.com>
2017-05-02 18:46:56 +0200

    webpack bundle

---
+++ app/index.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello and welcome to React!
+            </div>)
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
