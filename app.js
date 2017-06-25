d06756182f1208a79de7f8043f7ab923bd0f442d
thshr <ths_hr@hotmail.com>
2017-06-25 02:52:51 +0200

    added stats comp

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var ReactDOM = require('react-dom');
 class HelloWorld extends React.Component {
     render() {
         return (
-            <div>Hello World!</div>
+            <div>Hello World!!!</div>
         )
     }
 }
---
+++ app/components/Statistics.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ths on 25.6.2017..
+ */
+var React = require('react');
+
+class Statistics extends React.Component {
+    render(){
+        return(
+            <div>Statistics!</div>
+        )
+    }
+}
+
+module.exports = Statistics;
